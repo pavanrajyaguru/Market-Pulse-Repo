69b8fb33d6bee14d9f43bae575fe814ce76a04ce
Pavan Rajyaguru <pavanrajyaguru1001@gmail.com>
2024-03-09 16:56:37 +0530

    Link Templates
    - Added API : http://127.0.0.1:8000/getindices

--- MarketPulse_proj/MarketPulse_app/views.py
+++ MarketPulse_proj/MarketPulse_app/views.py
@@ -1,3 +1,25 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from nsepython import *
 # Create your views here.
+
+def home(request):
+    
+    return render(request,"index.html")
+
+
+def get_indices(request):
+    positions = nsefetch('https://www.nseindia.com/api/equity-stockIndices?index=SECURITIES%20IN%20F%26O')
+    
+    start = request.POST.get("start",0)
+    print("start", start)
+    end = request.POST.get("end",10)
+    print("end", end)
+  
+    response = positions["data"][int(start):int(end)]
+    
+    print(response[0].keys())
+    
+    response 
+    
+    return HttpResponse(json.dumps(response))
