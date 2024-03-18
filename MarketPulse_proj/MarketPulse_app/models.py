9ca0e7b00dc9aba9a1ccea565410b99fcf5ac8cd
Pavan Rajyaguru <pavanrajyaguru1001@gmail.com>
2024-03-18 22:37:53 +0530

    Model added Watch_list

--- MarketPulse_proj/MarketPulse_app/models.py
+++ MarketPulse_proj/MarketPulse_app/models.py
@@ -8,3 +8,9 @@ class User(models.Model):
     email = models.CharField(max_length=255)
     password = models.CharField(max_length=150)
     gender = models.CharField(max_length=50)
+    
+class Watch_list(models.Model):
+    user_id = models.ForeignKey("User", verbose_name=("user_id"), on_delete=models.CASCADE)
+    index = models.CharField(max_length=255)
+    w_name = models.CharField(max_length=100)
+    date_modified = models.DateTimeField(auto_now=True)
